9914571802ae7fdd37258521a12763de7ce0848d
Lerduzz <lbruzond@gmail.com>
2024-04-25 01:59:37 -0500

    Separado el DadosWorker.

--- parchis.py
+++ parchis.py
@@ -1,57 +1,9 @@
-import sys, time, random, qdarkstyle
+import sys, time, qdarkstyle
 from PyQt5.QtCore import QObject, QPoint, QThread, pyqtSignal
 from PyQt5.QtGui import QResizeEvent, QIcon, QPixmap, QFont
 from PyQt5.QtWidgets import QMainWindow, QApplication, QListWidgetItem, QMenu, QAction, QProxyStyle, QStyle
 from parchis_ui import Ui_VentanaJuego
-
-class DadosWorker(QObject):
-    finished = pyqtSignal(int, int)
-    progress = pyqtSignal(int, int)
-
-    def __init__(self, s1, s2):
-        super().__init__()
-        self.__s1 = s1
-        self.__s2 = s2
-        self.__r1 = random.randint(1, 6)
-        self.__r2 = random.randint(1, 6)
-
-    def run(self):
-        d1C = 0
-        d2C = 0
-        alt = random.randint(1, 6)
-        flag = 0
-        skip = random.randint(1, 2)
-        m1 = random.randint(2, 4)
-        m2 = random.randint(2, 4)
-        while d1C < m1 or d2C < m2:
-            if alt <= 3:
-                if flag >= skip:
-                    d1C += 1 if d1C < m1 and self.__s1 == self.__r1 else 0
-                    self.__s1 += 1 if d1C < m1 else 0
-                    self.__s1 = 1 if self.__s1 > 6 else self.__s1
-                    flag = 0
-                else:
-                    flag += 1
-            else:
-                d1C += 1 if d1C < m1 and self.__s1 == self.__r1 else 0
-                self.__s1 += 1 if d1C < m1 else 0
-                self.__s1 = 1 if self.__s1 > 6 else self.__s1
-            if alt >= 4:
-                if flag >= skip:
-                    d2C += 1 if d2C < m2 and self.__s2 == self.__r2 else 0
-                    self.__s2 += 1 if d2C < m2 else 0
-                    self.__s2 = 1 if self.__s2 > 6 else self.__s2
-                    flag = 0
-                else:
-                    flag += 1
-            else:
-                d2C += 1 if d2C < m2 and self.__s2 == self.__r2 else 0
-                self.__s2 += 1 if d2C < m2 else 0
-                self.__s2 = 1 if self.__s2 > 6 else self.__s2
-            self.progress.emit(self.__s1, self.__s2)
-            time.sleep(0.01)
-        self.finished.emit(self.__r1, self.__r2)
-
+from workers.dados import DadosWorker
 
 class TurnoWorker(QObject):
     started = pyqtSignal(int)
