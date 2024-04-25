66e9ff2b1840c43ee5078019c78d789774a03805
lerduzz <skxawngcu@gmail.com>
2024-04-24 22:48:57 -0500

    Nueva beta.

--- parchis_ui.py
+++ parchis_ui.py
@@ -268,7 +268,7 @@ class Ui_VentanaJuego(object):
 
     def retranslateUi(self, VentanaJuego):
         _translate = QtCore.QCoreApplication.translate
-        VentanaJuego.setWindowTitle(_translate("VentanaJuego", "Juego de Parchis | Creado con Python y PyQt | v0.6.7 Beta"))
+        VentanaJuego.setWindowTitle(_translate("VentanaJuego", "Juego de Parchis | Creado con Python y PyQt | v0.7.1 Beta"))
         self.cajaJugar.setTitle(_translate("VentanaJuego", "Jugar"))
         self.checkPlayer0.setText(_translate("VentanaJuego", "Rojo"))
         self.checkPlayer1.setText(_translate("VentanaJuego", "Verde"))
@@ -282,7 +282,7 @@ class Ui_VentanaJuego(object):
         __sortingEnabled = self.listHistorial.isSortingEnabled()
         self.listHistorial.setSortingEnabled(False)
         item = self.listHistorial.item(0)
-        item.setText(_translate("VentanaJuego", "Creado por Lerduzz (https://youtube.com/@lerduzz). Versión: v0.6.7 Beta."))
+        item.setText(_translate("VentanaJuego", "Creado por Lerduzz (https://youtube.com/@lerduzz). Versión: v0.7.1 Beta."))
         self.listHistorial.setSortingEnabled(__sortingEnabled)
         self.actionNuevaPartida.setText(_translate("VentanaJuego", "Nueva partida"))
         self.actionDosJugadores.setText(_translate("VentanaJuego", "Dos jugadores"))
