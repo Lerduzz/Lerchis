11fcc7c5fbb8f51c020510cb041eeea2f3a476fc
Lerduzz <lbruzond@gmail.com>
2024-04-25 23:32:19 -0500

    Prevenir error al cerrar la aplicacion.

--- workers/turno.py
+++ workers/turno.py
@@ -14,12 +14,15 @@ class TurnoWorker(QObject):
         self.__interval = 0.9
 
     def run(self):
-        self.started.emit(self.__max)
-        while self.__value < self.__max:
-            self.progress.emit(self.__value)
-            self.__value += 1
-            time.sleep(self.__interval)
-        self.finished.emit(self.__value)
+        try:
+            self.started.emit(self.__max)
+            while self.__value < self.__max:
+                self.progress.emit(self.__value)
+                self.__value += 1
+                time.sleep(self.__interval)
+            self.finished.emit(self.__value)
+        except RuntimeError:
+            pass
 
     def faster(self):
         self.__interval = 0.025
