dddb9195f04bb25a90f77642df832bc66241ccef
Lerduzz <lbruzond@gmail.com>
2024-04-27 18:31:01 -0500

    Mejorado el contador de turno.

--- workers/turno.py
+++ workers/turno.py
@@ -9,23 +9,23 @@ class TurnoWorker(QObject):
 
     def __init__(self, start):
         super().__init__()
-        self.__max = start
+        self.__max = start * 10
         self.__value = 0
-        self.__interval = 0.9
+        self.__interval = 0.1
 
     def run(self):
         try:
-            self.started.emit(self.__max)
+            self.started.emit(self.__max // 10)
             while self.__value < self.__max:
-                self.progress.emit(self.__value)
+                self.progress.emit(self.__value // 10)
                 self.__value += 1
                 time.sleep(self.__interval)
-            self.finished.emit(self.__value)
+            self.finished.emit(self.__value // 10)
         except RuntimeError:
             pass
 
     def faster(self):
-        self.__interval = 0.025
+        self.__interval = 0.0025
 
     def isFast(self) -> float:
-        return self.__interval == 0.025
+        return self.__interval == 0.0025
