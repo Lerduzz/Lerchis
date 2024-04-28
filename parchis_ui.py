d8a20bc8586565fb103b4d3acec2ef23b6cba6bb
Lerduzz <lbruzond@gmail.com>
2024-04-27 20:59:13 -0500

    Otra beta.

--- parchis_ui.py
+++ parchis_ui.py
@@ -298,7 +298,7 @@ class Ui_VentanaJuego(object):
         __sortingEnabled = self.listHistorial.isSortingEnabled()
         self.listHistorial.setSortingEnabled(False)
         item = self.listHistorial.item(0)
-        item.setText(_translate("VentanaJuego", "Creado por Lerduzz (https://youtube.com/@lerduzz). Versión: v0.8.3 Beta."))
+        item.setText(_translate("VentanaJuego", "Creado por Lerduzz (https://youtube.com/@lerduzz). Versión: v0.8.5 Beta."))
         self.listHistorial.setSortingEnabled(__sortingEnabled)
         self.actionNuevaPartida.setText(_translate("VentanaJuego", "Nueva partida"))
         self.actionDosJugadores.setText(_translate("VentanaJuego", "Dos jugadores"))
